138d6ad7c96cab4cc9103497429140775d5ba40d
Omo <omoikheloa56@gmail.com>
2023-12-08 11:30:00 -0800

    return description

--- split_token.c
+++ split_token.c
@@ -4,7 +4,8 @@
  * split_token - function that takes only the token and split them
  * without considering spaces and tabs
  * @input: input line or message
- * Return: buffer where there is the command
+ *
+ * Return: buffer containing the tokens passed from input or NULL
  */
 
 char **split_token(char *input)

14c6a086268d41fc8b37ff2bc2a6313247bee31e
Omo <omoikheloa56@gmail.com>
2023-12-13 11:02:10 -0800

    implement function update

--- _implement.c
+++ _implement.c
@@ -12,22 +12,31 @@ int _implement(char **buffer, char **av)
 	pid_t fork_child;
 	int position;
 	int exit_position;
+	char *full_cmd;
+
+	full_cmd = get_path(buffer[0]);
+	if (full_cmd)
+	{
+		printerror();
+
+		return();
+	}
 
 	fork_child = fork();
 
 	if (fork_child == 0)
 	{
-		if (execve(buffer[0], buffer, environ) == -1)
+		if (execve(full_cmd, buffer, environ) == -1)
 		{
-			perror(av[0]);
+			free(full_cmd), full_cmd = NULL;
 			free_2d(buffer);
-			exit(0);
 		}
 	}
 	else
 	{
 		waitpid(fork_child, &position, 0);
 		free_2d(buffer);
+		free(full_cmd), full_cmd = NULL;
 	}
 	exit_position = WEXITSTATUS(position);
 	return (exit_position);

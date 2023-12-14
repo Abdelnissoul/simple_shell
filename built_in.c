076d993a912a5e004bd7755e73de3e947a3cb4bf
Omo <omoikheloa56@gmail.com>
2023-12-13 20:42:53 -0800

    betty

--- built_in.c
+++ built_in.c
@@ -1,5 +1,12 @@
 #include "shell.h"
 
+/**
+ * is_builtin - determines if a command is built in or not
+ * @command: command to check
+ *
+ * Return: 1 if command is recognised, 0 if otherwise
+ */
+
 int is_builtin(char *command)
 {
 	char *builtin[] = {
@@ -14,27 +21,46 @@ int is_builtin(char *command)
 	}
 	return (0);
 }
+/**
+ * handle_builtin - handles the execution of buitin commands
+ * @command: command to be handled
+ * @av: argument vector
+ * @status: status of the command
+ * @idx: position of command in sequence
+ *
+ * Return: NULL
+ */
 
 void handle_builtin(char **command, char **av, int *status, int idx)
 {
-	
+
 	if (_strcmp(command[0], "exit") == 0)
 		exit_shell(command, av, status, idx);
 
 	else if (_strcmp(command[0], "env") == 0)
 		print_env(command, status);
 }
+/**
+ * exit_shell - terminates or exits the shell
+ * @command: action that triggers exit
+ * @av: argument vector
+ * @status: status of the shell
+ * @idx: position of the shell command
+ *
+ * Return: exit or 0
+ */
 
 void exit_shell(char **command, char **av, int *status, int idx)
 {
 	int exit_value = (*status);
-	char *index, msg[] = "exit: illegal number: ";
+	char *index = NULL;
+	char msg[] = "exit: illegal number: ";
 
 	if (command[1])
 	{
 		if (is_positive_number(command[1]))
 		{
-			exit_value = atoi(command[1]);
+			exit_value = _atoi(command[1]);
 		}
 		else
 		{
@@ -47,13 +73,22 @@ void exit_shell(char **command, char **av, int *status, int idx)
 			write(STDERR_FILENO, "\n", 1);
 			free_2d(command);
 			free(index);
-			(*status) = 2;
+			(*status) = 1;
+
 			return;
 		}
 	}
 	free_2d(command);
 	exit(exit_value);
 }
+
+/**
+ * print_env - prints environment variable within a shell
+ * @command: a pointer to a string array
+ * @status: status of the printing process
+ *
+ * Return: void
+ */
 
 void print_env(char **command, int *status)
 {
